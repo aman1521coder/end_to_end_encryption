6cbd000380b80c5204cabec7975dea3867946386
aman1521coder <amanesu1521@gmail.com>
2024-08-22 08:50:34 +0300

    third commit

--- chat/chatapp/models.py
+++ chat/chatapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+	userId=pass
+	Name=pass
+	password=pass
+	Gmail=pass
+	follower_ID
+	following_ID
+	public_key
+	last_online
+	joined_data
+	Update_at
+    Avatar_url
+class Chat:
+	chat_Id
+	chat_content
+	chat_Time_stamp
+	chat_Pic_Url
+	Chat_Recive_ID
+	chat_Send_id
+	chat_status
+class Random_chat:
+	Random_chat_ID
+	User_ID
+	Chat_User_ID
+	Start_Time
+	End_Time
+class Freinds:
+	user_Id
+	frends_ID
+	friend_Status
+	friend_user_ID
+
